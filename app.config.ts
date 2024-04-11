2b2c80f1e278943bb3ef8de6639f8e60dc31c278
Jorge Garduño <jorge@rem.tools>
2024-04-11 09:57:19 -0600

    chore: minor changes to test

--- app.config.ts
+++ app.config.ts
@@ -1,8 +1,8 @@
 // https://github.com/nuxt-themes/alpine/blob/main/nuxt.schema.ts
 export default defineAppConfig({
   alpine: {
-    title: 'Alpine',
-    description: 'The minimalist blog theme',
+    title: 'Jorge Garduño',
+    description: 'Software Architect, Full Stack Developer, and Tech Enthusiast.',
     image: {
       src: '/social-card-preview.png',
       alt: 'An image showcasing my project.',
@@ -11,20 +11,16 @@ export default defineAppConfig({
     },
     header: {
       position: 'right', // possible value are : | 'left' | 'center' | 'right'
-      logo: {
-        path: '/logo.svg', // path of the logo
-        pathDark: '/logo-dark.svg', // path of the logo in dark mode, leave this empty if you want to use the same logo
-        alt: 'alpine' // alt of the logo
-      }
+      logo: false
     },
     footer: {
       credits: {
-        enabled: true, // possible value are : true | false
+        enabled: false, // possible value are : true | false
         repository: 'https://www.github.com/nuxt-themes/alpine' // our github repository
       },
       navigation: true, // possible value are : true | false
-      alignment: 'center', // possible value are : 'none' | 'left' | 'center' | 'right'
-      message: 'Follow me on' // string that will be displayed in the footer (leave empty or delete to disable)
+      alignment: 'left', // possible value are : 'none' | 'left' | 'center' | 'right'
+      message: 'Contactame en'
     },
     socials: {
       twitter: 'nuxtlabs',
@@ -32,7 +28,7 @@ export default defineAppConfig({
       linkedin: {
         icon: 'uil:linkedin',
         label: 'LinkedIn',
-        href: 'https://www.linkedin.com/company/nuxtlabs'
+        href: 'https://www.linkedin.com/in/damnyorch'
       }
     },
     form: {
--- tokens.config.ts
+++ tokens.config.ts
@@ -1,4 +1,16 @@
+import theme from '@nuxt-themes/tokens/config'
 import { defineTheme } from 'pinceau'
 
 export default defineTheme({
+    alpine: {
+        body: {
+            backgroundColor: {
+                initial: '#ffe9d9',
+                dark: '#09090b'
+            }
+        }
+    },
+    color: {
+        primary: theme.color.orange
+    }
 })
